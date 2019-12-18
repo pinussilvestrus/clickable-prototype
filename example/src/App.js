dc2efc652963063ab9cd2dc666f405a26f7afca3
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-12-18 08:55:54 +0100

    chore(example): update

--- example/src/App.js
+++ example/src/App.js
@@ -9,13 +9,18 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <ViewContainer defaultView='view1'>
+        <h1><span>clickable-prototype</span> Demo</h1>
+        <ViewContainer defaultView='view1' className='container'>
           <View key='view1' screen={view1}>
             <HitBox
-              position={{y: 81, x: 644, width: 50, height: 20}}
+              position={{y: 365, x: 560, width: 330, height: 70}}
               to='view2' />
           </View>
-          <View key='view2' screen={view2} />
+          <View key='view2' screen={view2}>
+            <HitBox
+              position={{y: 500, x: 560, width: 330, height: 70}}
+              to='view1' />
+          </View>
         </ViewContainer>
       </div>
     )
